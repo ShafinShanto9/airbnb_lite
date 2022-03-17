f6ff7e60bdbdbc21c96b4a92394f4292d0cf5a33
shafin_shant0 <shafinshanto9@gmail.com>
2022-03-17 12:09:33 +0600

    header complete

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 const Home = () => {
@@ -9,7 +10,18 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <Header/>
+      <Header />
+      <Banner />
+      
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-lg font-bold sm:text-4xl sm:font-semibold pb-5'>Explore Nearby</h2>
+
+          {/* Data Fetch From Server */}
+
+          
+        </section>
+      </main>
     </div>
   )
 }
