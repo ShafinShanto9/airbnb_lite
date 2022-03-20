2c456d58a3bd08e4ab8c60324272f0171a9b0ba6
shafin_shant0 <shafinshanto9@gmail.com>
2022-03-20 14:51:04 +0600

    create card components

--- components/Banner.js
+++ components/Banner.js
@@ -1,23 +1,44 @@
-import React from 'react'
 import Image from 'next/image'
+import React from 'react'
+import ReactPlayer from 'react-player'
 
 const Banner = () => {
   return (
-      <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'> 
+    <>
+    <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] w-full'> 
           <Image
               src="https://links.papareact.com/0fm"
               layout='fill'
               objectFit='cover'
-          />
-
+        />
+        
+        <div className='absolute top-1/2 w-full flex items-center justify-center text-center' >
           <div className='absolute top-1/2 w-full text-center' >
               <p className='text-sm sm:text-lg'>Not Sure Where To Go? PERFECT!</p>
 
               <button className='text-purple-500 px-10 py-4 bg-white shadow-md rounded-full
               font-bold my-3 hover:shadow-lg active:scale-90 transition duration-150'>I'm Flexible</button>
-          </div>
-    </div>
+        </div>
+        </div>
+      </div>
+         
+    </>
   )
 }
 
 export default Banner
+
+
+          {/* <div className='py-30 px-20 bg-white border-2 relative'> 
+            
+            <ReactPlayer
+              url='https://www.youtube.com/watch?v=8U161oScmsQ'
+              type="video/mp4"
+              loop
+              controls={false}
+              muted
+              autoPlay
+              className="w-full h-full object-cover"
+            />
+              
+          </div> */}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import { MediumCard } from '../components/MediumCard'
+import SmallCard from '../components/smallCard'
 
-const Home = () => {
+const Home = ({exploreData, cardsData}) => {
   return (
     <div className="">
       <Head>
@@ -12,14 +14,30 @@ const Home = () => {
       
       <Header />
       <Banner />
-      
-      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+      {/* Explore Nearby Section */}
+      <main className='max-w-7xl mx-auto px-8 sm:px-16 '>
         <section className='pt-6'>
           <h2 className='text-lg font-bold sm:text-4xl sm:font-semibold pb-5'>Explore Nearby</h2>
-
-          {/* Data Fetch From Server */}
-
+          {/* Pull Data From Server */} 
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {
+            exploreData?.map(item => (
+              <SmallCard key={item.img} img={item.img} location={item.location} distance={ item.distance}/>
+            ))
+            }
+          </div>
+        </section>
           
+        {/* Live AnyWehre Section */}
+        <section>
+          <h2 className='text-lg font-bold sm:text-4xl sm:font-semibold py-8'>Live Aynwehre</h2>
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            {
+            cardsData?.map(card => (
+              <MediumCard key={ card.img} img={card.img} title={card.title}  />
+            ))
+          }
+          </div>
         </section>
       </main>
     </div>
@@ -27,3 +45,19 @@ const Home = () => {
 }
 
 export default Home
+
+// Api Fetching
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp')
+    .then(res => res.json())
+  
+  const cardsData = await fetch('https://links.papareact.com/zp1')
+    .then(res => res.json())
+  
+  return {
+    props: {
+      exploreData,
+      cardsData
+    }
+  }
+}
