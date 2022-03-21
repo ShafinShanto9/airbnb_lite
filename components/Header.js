382f2255b8a36098ad3326395593275dd7247db8
shafin_shant0 <shafinshanto9@gmail.com>
2022-03-21 15:01:53 +0600

    date pick adedd

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,29 @@
 import { GlobeAltIcon, MenuIcon, SearchIcon, UserCircleIcon } from '@heroicons/react/solid'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 const Header = () => {
+    const [searchInput, setSeachInput] = useState('')
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+
+
+    //Date Range Picker Confiquration
+    const  selectionRange =  {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection',
+    }
+    // Handle Date Time
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate) 
+        setEndDate(ranges.selection.endDate)
+    }
+    console.log(`start date is ${startDate} and end date is ${endDate}`)
+
   return (
       <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
 
@@ -17,7 +38,9 @@ const Header = () => {
 
           {/* Search Section */} 
           <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-md'>
-              <input className='pl-7 md:pl-5  bg-transparent outline-none flex-grow
+              <input value={searchInput}
+                onChange={(e) => setSeachInput(e.target.value)}
+                className='pl-7 md:pl-5  bg-transparent outline-none flex-grow
               text-sm text-gray-600 placeholder-gray-400' type="text" placeholder='Start Your Search' /> 
               <SearchIcon className='h-8 bg-red-400 text-white p-2 rounded-full hidden md:inline-flex md:mx-2'/>
           </div>
@@ -31,7 +54,22 @@ const Header = () => {
                  <MenuIcon className='h-6 '/> 
                  <UserCircleIcon className='h-6 '/> 
               </div>
-          </div>  
+          </div>
+          
+          {/* IF Search Input has value then render this screen */}
+          {
+              searchInput && (
+                  <div>
+                      <DateRangePicker
+                          ranges={[selectionRange]}
+                          minDate={new Date()}
+                          rangeColors={['#FD5861']}
+                          onChange={handleSelect}
+
+                      />
+                  </div>
+              )
+          }
           
     </header>
   )
